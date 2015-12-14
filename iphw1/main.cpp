8161742f7d96103542e697a513589238dd23c5c7
不安浩倫 <sovietreborn@sovietreborndemacbook-air.local>
2015-12-14 13:58:01 +0800

    Initial Commit

---
+++ iphw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  iphw1
+//
+//  Created by sovietreborn on 2015/12/14.
+//  Copyright © 2015年 sovietreborn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
